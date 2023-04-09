5c0795ae7a27b55a644b2559efe9314a40a7c72d
Lovina Osaigbovo <111295018+LovinaOsaigbovo@users.noreply.github.com>
2023-04-10 00:32:26 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,43 +1,29 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 1-pack_web_static.py) that
-distributes an archive to your web servers, using the function
-do_deploy."""
-
-from datetime import datetime
-from fabric.api import *
-import shlex
-import os
-
+"""Fabric script that distributes an archive to your web servers"""
+import os.path
+from fabric.api import env, put, run, local
 
 env.hosts = ['3.90.83.66', '100.25.205.254']
-env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """ Deploys """
-    if not os.path.exists(archive_path):
+    """Distributes an archive to your web servers"""
+    if not os.path.isfile(archive_path):
         return False
+
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
+        put(archive_path, '/tmp/')
+        filename = os.path.basename(archive_path)
+        dirname = '/data/web_static/releases/' + filename[:-4]
+        run('mkdir -p {}'.format(dirname))
+        run('tar -xzf /tmp/{} -C {}'.
+            format(filename, dirname))
+        run('rm /tmp/{}'.format(filename))
+        run('mv {}/web_static/* {}/'.format(dirname, dirname))
+        run('rm -rf {}/web_static'.format(dirname))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.
+            format(dirname))
         return True
     except:
         return False
